026594adefea132a0deeb5721597d810a284645d
mishelrodri <rp18027@ues.edu.sv>
2024-04-09 17:57:30 -0600

    🌱 Initializing project

---
+++ src/main/java/com/matinsa/backend/MatinsaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.matinsa.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatinsaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatinsaBackendApplication.class, args);
+	}
+
+}
